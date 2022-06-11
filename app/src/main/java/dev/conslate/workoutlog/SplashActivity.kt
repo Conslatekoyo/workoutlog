cd9b158c2cd79caeeb9af428854940af82138c11
Conslatekoyo <conslatekoyo@gmail.com>
2022-06-11 10:23:48 +0300

    splash changes

--- app/src/main/java/dev/conslate/workoutlog/SplashActivity.kt
+++ app/src/main/java/dev/conslate/workoutlog/SplashActivity.kt
@@ -10,8 +10,7 @@ class SplashActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_splash)
          startActivity(Intent(this,LoginActivity::class.java))
-//        startActivity(intent)
-//        startActivity(intent)
+//
           finish()
     }
 }
